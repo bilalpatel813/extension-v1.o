ce3da2c7aa9211e7995a6533292a0798438f732b
bilalpatel813 <Bilalpatel78600@gmail.com>
2026-07-29 18:50:34 +0530

    testing

--- Pursuit/Backend/config/settings.py
+++ Pursuit/Backend/config/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
+#DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["*"]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
